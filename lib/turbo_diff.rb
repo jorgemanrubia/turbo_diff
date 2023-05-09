4dc069889b27100a08bb4f68ed1bd4c2874925f7
Jorge Manrubia <jorge@hey.com>
2023-05-09 16:10:33 +0200

    Add zeitwerk

--- lib/turbo_diff.rb
+++ lib/turbo_diff.rb
@@ -1,6 +1,10 @@
 require "turbo_diff/version"
 require "turbo_diff/railtie"
 
+require "zeitwerk"
+loader = Zeitwerk::Loader.for_gem
+loader.setup
+
 module TurboDiff
   # Your code goes here...
 end
